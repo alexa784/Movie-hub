795e0bd75577b70cc960bd34cbb13a49abb5ed51
Aleksandar Janjic <aleksandarjanjicng@gmail.com>
2025-02-18 18:02:42 +0100

    Added Movie Grid

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const MovieGrid = () => {
-  return <div>MovieGrid</div>;
-};
+const dummy = [0, 1, 2, 3, 4, 5, 6, 7];
 
+const MovieGrid = () => {
+  return (
+    <div className="container">
+      <div className="row row-cols-sm-1 row-cols-lg-2 row-cols-xl-3">
+        {dummy.map((v) => (
+          <div className="col" key={v}>
+            {v}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+/**Vidjeti na bootstrap 5 gridu kako ovo funkcionise*/
 export default MovieGrid;
